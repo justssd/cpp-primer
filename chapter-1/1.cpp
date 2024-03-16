be46979f36957f72cf4e9cc3a0ef94df5172cd5b
SSD <srinivass2024@gmail.com>
2024-03-15 21:44:37 -0700

    Section 1.2 Exercise 1.4

--- chapter-1/1.cpp
+++ chapter-1/1.cpp
@@ -2,6 +2,9 @@
 
 int main()
 {
-	std::cout << "Hello, World" << std::endl;
+	std::cout << "Enter two numbers:" << std::endl;
+	int v1 = 0, v2 = 0;
+	std::cin >> v1 >> v2;
+	std::cout << "The product of " << v1 << " and " << v2 << " is " << v1 * v2 << std::endl;
 	return 0;
 }
