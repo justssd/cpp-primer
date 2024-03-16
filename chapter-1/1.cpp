3b22c20a72eb97b10ef728a62c0a36a06ba59026
SSD <srinivass2024@gmail.com>
2024-03-16 14:09:20 -0700

    Section 1.4.1 Exercise 1.10

--- chapter-1/1.cpp
+++ chapter-1/1.cpp
@@ -2,13 +2,10 @@
 
 int main()
 {
-	int sum = 0, val = 50;
-	// keep executing the while as long as val is less than or equal to 10
-	while (val <= 100) {
-		sum += val;	// assigns sum + val to sum
-		++val;		// add 1 to val
+	int val = 10;
+	while (val >= 0) {
+		std::cout << val << std::endl;
+		--val;
 	}
-	std::cout << "Sum of 50 to 100 inclusive is "
-		  << sum << std::endl;
 	return 0;
 }
