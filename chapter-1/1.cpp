9dfb0dc77cfb589ef617b438abba39baba569607
SSD <srinivass2024@gmail.com>
2024-03-15 21:34:43 -0700

    Section 1.2 Exercise 1.3

--- chapter-1/1.cpp
+++ chapter-1/1.cpp
@@ -2,9 +2,6 @@
 
 int main()
 {
-	std::cout << "Enter two numbers:" << std::endl;
-	int v1 = 0, v2 = 0;
-	std::cin >> v1 >> v2;
-	std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+	std::cout << "Hello, World" << std::endl;
 	return 0;
 }
