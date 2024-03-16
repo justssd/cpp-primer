58de5b83d79bd2d3f4ee6df26dc90ed5f4323788
SSD <srinivass2024@gmail.com>
2024-03-16 08:09:34 -0700

    Section 1.3 Exercise 1.7

--- chapter-1/1.cpp
+++ chapter-1/1.cpp
@@ -1,21 +1,10 @@
-#include <iostream>
+/*
+*  comment pairs /* */ cannot nest.
+* "cannot nest" is considered source code,
+* as is the rest of the program
+*/
 
 int main()
 {
-	std::cout << "Enter two numbers:";
-	std::cout << std::endl;
-	int v1 = 0, v2 = 0;
-	std::cin >> v1 >> v2;
-	std::cout << "The product of ";
-	std::cout << v1;
-	std::cout << " and ";
-	std::cout << v2;
-	std::cout << " is ";
-		  << v1 * v2;
-		  << std::endl;
-	// Not legal.
-	// error: expected primary-expression before ‘<<’ token
-	// << is intended to be an output stream operator in this specific context.
-	// Prepend std::cout to fix.
 	return 0;
 }
